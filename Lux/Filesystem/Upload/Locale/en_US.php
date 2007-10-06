1faaec251157fc48f4da232d735e3cfe794a0829
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2007-10-06 13:45:33 +0000

    Adding locale files.
    git-svn-id: http://lux.googlecode.com/svn/trunk@46 2aec7fc6-ba36-0410-9347-2924fe5736e0

---
+++ Lux/Filesystem/Upload/Locale/en_US.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Locale file. Returns the strings for a specific language.
+ *
+ * @category Lux
+ *
+ * @package Lux_Filesystem_Upload
+ *
+ * @author Rodrigo Moraes <rodrigo.moraes@gmail.com>
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ * @version $Id$
+ *
+ */
+return array(
+    'ERR_DIR_NOT_FOUND'   => 'Directory not found.',
+    'ERR_DIR_PERMISSION'  => 'Wrong directory permission.',
+    'ERR_FILE_EXISTS'     => 'File already exists in directory.',
+    'ERR_FILE_PERMISSION' => 'Wrong file permission.',
+    'ERR_FILE_EXTENSION'  => 'Extension not allowed.',
+    'ERR_FILE_TYPE'       => 'Wrong file type.',
+    'ERR_FILE_SIZE'       => 'Maximum size limit exceed.',
+    'ERR_UPLOAD_FAILED'   => 'Upload failed.',
+);
---
+++ Lux/Filesystem/Upload/Locale/pt_BR.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Locale file. Returns the strings for a specific language.
+ *
+ * @category Lux
+ *
+ * @package Lux_Filesystem_Upload
+ *
+ * @author Rodrigo Moraes <rodrigo.moraes@gmail.com>
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ * @version $Id$
+ *
+ */
+return array(
+    'ERR_DIR_NOT_FOUND'   => 'Diretório não encontrado.',
+    'ERR_DIR_PERMISSION'  => 'O diretório não tem permissão de escrita.',
+    'ERR_FILE_EXISTS'     => 'Um arquivo com o mesmo nome já existe.',
+    'ERR_FILE_PERMISSION' => 'O arquivo não tem permissão de escrita.',
+    'ERR_FILE_EXTENSION'  => 'Extensão não permitida.',
+    'ERR_FILE_TYPE'       => 'O tipo do arquivo não é permitido.',
+    'ERR_FILE_SIZE'       => 'Tamanho máximo do arquivo excedido.',
+    'ERR_UPLOAD_FAILED'   => 'O envio do arquivo falhou.',
+);
