7955a8626145ce0a2ea5c51f40925094cc0dce29
Antti Holvikari <anttih@gmail.com>
2007-10-07 13:49:06 +0000

    locale: added locale strings for fi_FI (finnish)
    git-svn-id: http://lux.googlecode.com/svn/trunk@49 2aec7fc6-ba36-0410-9347-2924fe5736e0

---
+++ Lux/Filesystem/Upload/Locale/fi_FI.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Locale file. Returns the strings for a specific language.
+ *
+ * @category Lux
+ *
+ * @package Lux_Filesystem_Upload
+ *
+ * @author Antti Holvikari <anttih@gmail.com>
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ * @version $Id: en_US.php 48 2007-10-07 13:26:34Z anttih $
+ *
+ */
+return array(
+    'ERR_DIR_NOT_FOUND'    => 'Hakemisto ei löydy.',
+    'ERR_DIR_PERMISSIONS'  => 'Väärät hakemisto-oikeudet.',
+    'ERR_FILE_EXISTS'      => 'Tiedosto on jo olemassa.',
+    'ERR_FILE_PERMISSIONS' => 'Väärät tiedoston käyttöoikeudet.',
+    'ERR_FILE_EXTENSION'   => 'Väärä tiedostopääte.',
+    'ERR_FILE_TYPE'        => 'Väärä tiedostoformaatti.',
+    'ERR_FILE_SIZE'        => 'Tiedoston koko on liian iso.',
+    'ERR_UPLOAD_FAILED'    => 'Tiedoston lähetys epäonnistui.',
+);
