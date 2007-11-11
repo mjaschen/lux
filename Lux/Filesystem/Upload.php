906bb6daf293408af00d9d3b48f879d754b11009
Antti Holvikari <anttih@gmail.com>
2007-11-11 16:01:32 +0000

    refactored upload class
    git-svn-id: http://lux.googlecode.com/svn/trunk@76 2aec7fc6-ba36-0410-9347-2924fe5736e0

--- Lux/Filesystem/Upload.php
+++ Lux/Filesystem/Upload.php
@@ -69,83 +69,77 @@ class Lux_Filesystem_Upload extends Solar_Base
      */
     protected $_Lux_Filesystem_Upload = array(
         'filesystem'  => 'Lux_Filesystem',
-        'destination' => null,
+        'file'        => 'userfile',
         'extensions'  => array(),
         'max_size'    => 40960,
-        'replace'     => false,
         'permission'  => 0444,
     );
-
+    
     /**
-     *
-     * Posted file from $_FILE.
-     *
-     * @var array
-     *
-     */
-    protected $_file;
-
-    /**
-     *
-     * Destination file name.
-     *
-     * @var string
-     *
-     */
-    protected $_file_name;
-
-    /**
-     *
-     * Extracted file extension.
-     *
-     * @var string
-     *
-     */
-    protected $_extension;
-
-    /**
-     *
-     * Options for the uploaded file, merged with the config.
-     *
-     * @var array
-     *
-     */
-    protected $_spec;
-
-    /**
-     *
-     * Initializes the upload.
-     *
-     * Validates that the upload is infact an uploaded file
      * 
-     * @param array $file Posted file from $_FILE.
+     * Invalidation feedback messages
+     * 
+     * @var array
+     * 
+     */
+    protected $_invalid = array();
+    
+    /**
      *
-     * @param array $spec Options for the uploaded file, merged with the config.
+     * Info from $_FILES
      *
-     * @return array Upload specification data.
+     * @var array
      *
      */
-    public function init($file, $spec = null)
+    public $file;
+    
+    /**
+     * 
+     * Validates upload and sets the environment
+     * for further file processing
+     * 
+     * @return bool true is validation succeeds; false if
+     * fails
+     * 
+     */
+    public function validate($key = null)
     {
-        // Sets the file and extension.
-        $this->_setFile($file);
-
-        // Set the specification.
-        $this->_setSpec($spec);
-
-        if(is_uploaded_file($this->_file['tmp_name'])) {
-            // validate extension, mime-type and size
-            $this->_validateExtension();
-            $this->_validateMimeType();
-            $this->_validateSize();
+        // get the request object
+        $request = Solar::factory('Solar_Request');
+        
+        $file = $this->_config['file'];
+        if (! empty($key)) {
+            $file = $key;
         }
-
-        // Add the destination file name to the spec.
-        $this->_spec['file_name'] = $this->_file_name;
-
-        return $this->_spec;
+        
+        // get info from $_FILES
+        $this->file = $request->files($file, false);
+        
+        // proceed with validations?
+        if($this->file && is_uploaded_file($this->file['tmp_name'])) {
+            
+            $filesystem = Solar::factory($this->_config['filesystem']);
+            
+            // set file extension
+            $this->_extension = $filesystem->getExtension($this->file['name']);
+            
+            // perform all validation
+            $this->_validate();
+            
+        } else {
+            // it was not even an uploaded file
+            $this->_invalid[] = $this->locale('ERR_UPLOAD_FILE');
+        }
+        
+        // invalids?
+        if (! empty($this->_invalid)) {
+            return false;
+        }
+        
+        // all ok and we can proceed with the file
+        return true;
     }
-
+    
     /**
      *
      * Move file to destination
@@ -153,196 +147,113 @@ class Lux_Filesystem_Upload extends Solar_Base
      * @return void
      *
      */
-    public function moveFile()
+    public function moveFile($dest)
     {
-        $this->_validateFile();
-        $this->_validateDir();
-
-        // Formats the file name.
-        $this->_formatFileName();
-
-        $destination = $this->_spec['destination']
-                     . DIRECTORY_SEPARATOR
-                     . $this->_file_name;
-
-        // Move file and chmod.
-        if(@move_uploaded_file($this->_file['tmp_name'], $destination)) {
-            @chmod($destination, $this->_spec['permission']);
+        // this will throw an exception
+        $this->_checkDestination($dest);
+        
+        // attempt to move
+        if(@move_uploaded_file($this->file['tmp_name'], $target)) {
+            @chmod($dest, $this->_config['permission']);
         } else {
             throw $this->_exception('ERR_MOVE');
         }
     }
-
+    
     /**
-     *
-     * Get info about the upload
-     *
+     * 
+     * Returns invalidation feedback messages
+     * 
      * @return void
-     *
+     * 
      */
-    public function getTmpFileInfo()
+    public function getInvalid()
     {
-        return $this->_file;
+        return $this->_invalid;
     }
-
+    
     /**
-     *
-     * Sets specifications for each uploaded file.
-     *
+     * 
+     * Checks if the destination file is writable and
+     * the file can be wrote to it
+     * 
      * @return void
-     *
+     * 
      */
-    protected function _setSpec($spec = null)
+    protected function _checkDestination($dest)
     {
-        if($spec) {
-            $this->_spec = array_merge($this->_config, $spec);
-        } else {
-            $this->_spec = $this->_config;
+        if (! is_writable($dest)) {
+            throw $this->_exception('ERR_DIR_PERMISSIONS', $dest);
         }
-
-        // Convert extensions and myme-types to lower case.
-        $ext = array();
-
-        foreach($this->_spec['extensions'] as $name => $mime) {
-            $ext[strtolower($name)] = array_map('strtolower', (array) $mime);
-        }
-
-        $this->_spec['extensions'] = $ext;
     }
-
-    /**
-     *
-     * Sets the uploaded file and extracts its extension.
-     *
-     * @return void
-     *
-     */
-    protected function _setFile($file)
-    {
-        $this->_file = $file;
-        $filesystem = Solar::factory($this->_config['filesystem']);
-        $this->_extension = strtolower($filesystem->getExtension($file['name']));
-    }
-
-    /**
-     *
-     * Formats a file name to be saved. By default sets as the uploaded file
-     * name; extend this to add other possibilities.
-     *
-     * @return void
-     *
-     */
-    protected function _formatFileName()
-    {
-        $this->_file_name = $this->_file['name'];
-    }
-
+    
     // -----------------------------------------------------------------
     //
     // Validation
     //
     // -----------------------------------------------------------------
-
+    
     /**
-     *
-     * Validate uploaded file and destination directory.
-     *
-     * @return bool True if it passes all checkings.
-     *
+     * 
+     * Validates uploaded file and sets feedback
+     * 
+     * @return void
+     * 
      */
     protected function _validate()
     {
+        // validate extension, mime-type and size
         $this->_validateExtension();
         $this->_validateMimeType();
         $this->_validateSize();
-        $this->_validateDir();
-        $this->_validateFile();
     }
-
+    
     /**
      *
-     * Validates file extension.
+     * Validates file extension
      *
      * Throws an exception if it doesn't validate.
      *
      */
     protected function _validateExtension()
     {
-        if(!isset($this->_spec['extensions'][$this->_extension])) {
-            throw $this->_exception('ERR_FILE_EXTENSION', $this->_extension);
+        if (! isset($this->_config['extensions'][$this->_extension])) {
+            $this->_invalid[] = $this->locale('ERR_FILE_EXTENSION');
         }
     }
-
+    
     /**
-     *
+     * 
      * Validates file mime type.
-     *
+     * 
      * Throws an exception if it doesn't validate.
-     *
+     * 
      */
     protected function _validateMimeType()
     {
-        $mime_types = (array) $this->_spec['extensions'][$this->_extension];
-
-        if(! in_array($this->_file['type'], $mime_types)) {
-            throw $this->_exception('ERR_FILE_TYPE', $this->_file['type']);
+        $ext = array_key_exists($this->_extension, $this->_config['extensions']);
+        
+        if (! $ext
+        || ! in_array(
+            $this->file['type'],
+            $this->_config['extensions'][$this->_extension])) {
+                
+            $this->_invalid[] = $this->locale('ERR_FILE_TYPE');
         }
     }
-
+    
     /**
-     *
-     * Validates file size.
-     *
-     * Throws an exception if it doesn't validate.
-     *
+     * 
+     * Validates that file size is equal to or smaller than
+     * set in the config key `max_size`
+     * 
+     * @return void
+     * 
      */
     protected function _validateSize()
     {
-        if($this->_file['size'] > $this->_spec['max_size']) {
-            throw $this->_exception('ERR_FILE_SIZE', $this->_file['size']);
-        }
-    }
-
-    /**
-     *
-     * Validates that the destination directory exists and is writable.
-     *
-     * Throws an exception if it doesn't validate.
-     *
-     */
-    protected function _validateDir()
-    {
-        $path = $this->_spec['destination'];
-
-        if(!file_exists($path)) {
-            throw $this->_exception('ERR_DIR_NOT_FOUND', $path);
-        } elseif(!is_writable($path)) {
-            throw $this->_exception('ERR_DIR_PERMISSIONS', $path);
-        }
-    }
-
-    /**
-     *
-     * Validates, if the destination file exists, that it can be replaced
-     * and is writable.
-     *
-     * Throws an exception if it doesn't validate.
-     *
-     */
-    protected function _validateFile()
-    {
-        $path = $this->_spec['destination'] . DIRECTORY_SEPARATOR
-            . $this->_file_name;
-
-        if(file_exists($path)) {
-            // Allow file replacement?
-            if(!$this->_spec['replace']) {
-                throw $this->_exception('ERR_FILE_EXISTS', $path);
-            }
-            // Has permission to replace existent file?
-            elseif(!is_writable($path)) {
-                throw $this->_exception('ERR_FILE_PERMISSION', $path);
-            }
+        if ($this->file['size'] > $this->_config['max_size']) {
+            $this->_invalid[] = $this->locale('ERR_FILE_SIZE');
         }
     }
 }
