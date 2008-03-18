8f8c1de5465ab5dd9419459eb2936c94528bfaeb
Antti Holvikari <anttih@gmail.com>
2008-03-18 19:11:02 +0200

    Lux_Git_Commit: [NEW] New class representing one commit

---
+++ Lux/Git/Commit.php
@@ -0,0 +1,16 @@
+<?php
+
+class Lux_Git_Commit extends Solar_Struct {
+    
+    /**
+     * 
+     * undocumented class variable
+     * 
+     * @var string
+     * 
+     */
+    protected $_Lux_Git_Commit = array(
+        'repo' => array('Lux_Git_Repo'),
+        'data' => array(),
+    );
+}
